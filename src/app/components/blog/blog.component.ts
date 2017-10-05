72cf1cdcffab2a8f04bc6d098d516c030305ea46
landon.gavin <landon.gavin@gmail.com>
2017-10-05 16:03:59 -0400

    Styling update/More mobile friendly

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -3,6 +3,7 @@ import {FirebaseService} from '../../services/firebase.service';
 import {Subscription} from 'rxjs/Subscription';
 import {Blog} from '../../interfaces/blog';
 import {Card} from '../../interfaces/card';
+import {MediaChange, ObservableMedia} from '@angular/flex-layout';
 
 @Component({
   selector: 'app-blog',
@@ -13,8 +14,12 @@ export class BlogComponent implements OnInit, OnDestroy {
 
   waiting = true;
   failed = false;
+  cardSize = 0;
+  mediaAlias: string;
 
   private blogSubscription: Subscription;
+  private mediaSubscription: Subscription;
+
   blogList: Blog[];
   private videoList: string[] = [
     'ogv', 'ogm', 'ogg', 'mp4', 'webm', 'gifv'
@@ -24,9 +29,20 @@ export class BlogComponent implements OnInit, OnDestroy {
     'png', 'bmp', 'jpeg', 'jpg', 'gif'
   ];
 
-  constructor(private firebaseService: FirebaseService) { }
+  constructor(private firebaseService: FirebaseService,  private observableMedia: ObservableMedia) {
+    this.mediaSubscription = this.observableMedia.subscribe((mediaAlias: MediaChange) => {
+      this.mediaAlias = mediaAlias.mqAlias;
+      this.loadMobileContent();
+    });
+  }
 
   ngOnInit() {
+    console.log(this.observableMedia.isActive('xs'), this.observableMedia.isActive('sm'), this.observableMedia.isActive('md'), this.observableMedia.isActive('lg'));
+
+
+      this.loadMobileContent();
+
+
     this.blogSubscription = this.firebaseService.getBlogPostsFromFB().subscribe(blogList => {
       this.blogList = this.sortBlogArrayByIdDesc(this.parseCardArrayToBlogArray(blogList));
       this.waiting = false;
@@ -36,9 +52,21 @@ export class BlogComponent implements OnInit, OnDestroy {
     });
   }
 
+  private loadMobileContent(): void {
+    if (this.observableMedia.isActive('xs')) {
+      console.log('here');
+      this.cardSize = 1;
+    } else {
+      this.cardSize = 0;
+    }
+  }
+
   ngOnDestroy() {
     if (this.blogSubscription) {
       this.blogSubscription.unsubscribe();
+    }
+    if (this.mediaSubscription) {
+      this.mediaSubscription.unsubscribe();
     }
   }
 
@@ -79,9 +107,11 @@ export class BlogComponent implements OnInit, OnDestroy {
         videoType: ''
       };
       if (blog.image.length > 0) {
-        const extension: string = card.image.substr(card.image.length - 3).toLocaleLowerCase();
+        const extension: string = card.image.substr(card.image.lastIndexOf('.') + 1).toLowerCase();
+        // const extension: string = card.image.substr(card.image.length - 3).toLocaleLowerCase();
         let allowed = false;
         for (const type of this.videoList) {
+          console.log(type, extension);
           if (extension === type) {
             blog.hasVideo = true;
             if (type === 'ogv' || type === 'ogm') {
