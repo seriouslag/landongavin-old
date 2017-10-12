e9f5d8e22eadea48717d484559ca7e73548d68db
Landon Gavin <landon.gavin@gmail.com>
2017-10-12 13:15:13 -0400

    fixed prod vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '<your-key>',
-    authDomain: '<your-project-authdomain>',
-    databaseURL: '<your-database-URL>',
-    projectId: '<your-project-id>',
-    storageBucket: '<your-storage-bucket>',
-    messagingSenderId: '<your-messaging-sender-id>'
+    apiKey: 'AIzaSyA_JxOTtrw4ioJu_op5M7H4f8Z5eZU2-tA',
+    authDomain: 'landongavin-72b35.firebaseapp.com',
+    databaseURL: 'https://landongavin-72b35.firebaseio.com',
+    projectId: 'landongavin-72b35',
+    storageBucket: 'landongavin-72b35.appspot.com',
+    messagingSenderId: '621260188514'
   }
 };
