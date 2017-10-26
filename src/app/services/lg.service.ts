7ed80fa127cde159eb78b6c31ab67bb7ce81b848
landon.gavin <landon.gavin@gmail.com>
2017-10-26 15:32:14 -0400

    removed logs; missing ;

--- src/app/services/lg.service.ts
+++ src/app/services/lg.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {Subscription} from 'rxjs/Subscription';
 import {Observable} from 'rxjs/Rx';
-import { FirebaseService } from './firebase.service'
+import { FirebaseService } from './firebase.service';
 
 @Injectable()
 export class LgService {
@@ -15,11 +15,9 @@ export class LgService {
 
   constructor(private firebaseService: FirebaseService) {
     this.editModeSubscription = this.editMode.subscribe(editMode => {
-      console.log('editMode in service', editMode);
       if (editMode === true && firebaseService.isVerified() !== true) {
         this.editModeIntervalSubscription = Observable.interval(3000).subscribe(() => {
           this.firebaseService.refreshUser();
-          console.log('Refreshing user');
         });
       } else {
         console.log('not refreshing user');
