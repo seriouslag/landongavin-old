8b2c137dc83d97f70fbcaa7540540ad52e168181
landon.gavin <landon.gavin@gmail.com>
2017-10-26 15:34:19 -0400

    moved routes to use lazy loaded modules

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,18 +1,20 @@
 import {RouterModule, Routes} from '@angular/router';
-import {AboutPageComponent} from './pages/about/about.page.component';
-import {HomePageComponent} from './pages/home/home.page.component';
 import {Four04Component} from './pages/four04/four04.component';
-import {SplashPageComponent} from './pages/splash/splash.page.component';
-import {AccountPageComponent} from './pages/account/account.page.component';
+
 
 export const appRoutes: Routes = [
-  {path: 'home', component: HomePageComponent},
+  {path: 'home', loadChildren: 'app/pages/home/home.page.module#HomePageModule'},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'about', redirectTo: 'about/LandonGavin', pathMatch: 'full'},
-  {path: 'splash', component: SplashPageComponent},
-  {path: 'about/:vanity', component: AboutPageComponent},
-  {path: 'account', component: AccountPageComponent},
-  {path: '**', component: Four04Component}
+  {path: 'splash', loadChildren: 'app/pages/splash/splash.page.module#SplashPageModule'},
+  {path: 'about/:vanity', loadChildren: 'app/pages/about/about.page.module#AboutPageModule'},
+  {path: 'account', loadChildren: 'app/pages/account/account.page.module#AccountPageModule'},
+
+  /*
+    Lazy loading with a wildcard does not work with angular4 yet.
+    {path: '**', loadChildren: 'app/pages/splash/splash.page.module#SplashPageModule'}
+   */
+  {path: '**', component: Four04Component},
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
