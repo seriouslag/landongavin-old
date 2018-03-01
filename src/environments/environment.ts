8fe8e1fe855439c8c196f85a8de748c576b074c7
Landon Gavin <landon.gavin@gmail.com>
2018-03-01 16:08:52 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,5 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyA_JxOTtrw4ioJu_op5M7H4f8Z5eZU2-tA',
-    authDomain: 'landongavin-72b35.firebaseapp.com',
-    databaseURL: 'https://landongavin-72b35.firebaseio.com',
-    projectId: 'landongavin-72b35',
-    storageBucket: 'landongavin-72b35.appspot.com',
-    messagingSenderId: '621260188514'
   }
 };
